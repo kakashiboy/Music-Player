67086c106a6b19e4477c71d50b4406d2a1b53d28
kakashiboy <jappanjapanese@gmail.com>
2024-11-08 19:50:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_USERNAME = getenv("BOT_USERNAME", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "8125003821:AAF43mHBu7ergH08eX1It8kpS1dXTU2PIcA")
+BOT_USERNAME = getenv("BOT_USERNAME", "https://t.me/Anime_Vc_Music_Bot")
 admins = {}
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "8726992"))
+API_HASH = getenv("API_HASH", "fbf4bc635f74937b9669af1d715171c9")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
